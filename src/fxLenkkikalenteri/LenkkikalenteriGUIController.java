26494d11c4bb80b68ff504b0df13425f3f4faa4a
kolarjjy <juuso.j.kolari@student.jyu.fi>
2022-02-12 00:37:24 +0200

    Kaikki valikot aukeaa, css muokkauksia

--- src/fxLenkkikalenteri/LenkkikalenteriGUIController.java
+++ src/fxLenkkikalenteri/LenkkikalenteriGUIController.java
@@ -1,16 +1,22 @@
 package fxLenkkikalenteri;
 
+import java.io.File;
 import java.io.IOException;
 
+import fi.jyu.mit.fxgui.Dialogs;
 import fi.jyu.mit.fxgui.ModalController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +25,12 @@ import javafx.stage.Stage;
  * pääikkunan kontrolleri
  */
 public class LenkkikalenteriGUIController {
+    
+    @FXML 
+    private MenuItem avaaTiedostoValikko;
+    
+    @FXML
+    private MenuItem avaaTilastoValikko;
 	
     @FXML
     private HBox paaIkkuna;
@@ -26,18 +38,84 @@ public class LenkkikalenteriGUIController {
     @FXML
     private MenuBar ylaValikko;
     
+    /* Yläpalkin toiminnot */
+    
+    @FXML
+    void handleAvaaTiedostoValikko() {
+      FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Avaa tiedosto"); 
+      fileChooser.getExtensionFilters().add(
+      new ExtensionFilter("Dat tiedostot", "*.dat"));
+      File valittuTiedosto = fileChooser.showOpenDialog(null);
+      if (valittuTiedosto != null) {
+          return;
+      }
+      Dialogs.showMessageDialog("tiedostoa ei voi avata, valitse toinen tiedosto");          
+    }
+    
+    @FXML
+    void handleAvaaTilastoValikko() {
+        try {
+            BorderPane panenaytatilastot = (BorderPane)FXMLLoader.load(getClass().getResource("Tilastot.fxml"));
+            Scene scenenaytatilastot = new Scene(panenaytatilastot,400,300);
+            scenenaytatilastot.getStylesheets().add(getClass().getResource("lenkkikalenteri.css").toExternalForm());
+            Stage stagenaytatilastot = new Stage();
+            stagenaytatilastot.setScene(scenenaytatilastot);
+            stagenaytatilastot.setResizable(false);
+            stagenaytatilastot.getIcons().add(new Image("/juoksija.png"));
+            stagenaytatilastot.setTitle("Tilastot");
+            stagenaytatilastot.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+    
+    @FXML
+    void handleAvaaTiedotValikko() {
+        try {
+            BorderPane panenaytatiedot = (BorderPane)FXMLLoader.load(getClass().getResource("Tietoja.fxml"));
+            Scene scenenaytatiedot = new Scene(panenaytatiedot,250,200);
+            scenenaytatiedot.getStylesheets().add(getClass().getResource("lenkkikalenteri.css").toExternalForm());
+            Stage stagenaytatiedot = new Stage();
+            stagenaytatiedot.setScene(scenenaytatiedot);
+            stagenaytatiedot.setResizable(false);
+            stagenaytatiedot.getIcons().add(new Image("/juoksija.png"));
+            stagenaytatiedot.setTitle("Tilastot");
+            stagenaytatiedot.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }    
+    }
+    
+
+    @FXML
+    void handleTulostaValikko() {
+        try {
+            BorderPane panetulosta = (BorderPane)FXMLLoader.load(getClass().getResource("Tulosta.fxml"));
+            Scene scenetulosta = new Scene(panetulosta,300,400);
+            scenetulosta.getStylesheets().add(getClass().getResource("lenkkikalenteri.css").toExternalForm());
+            Stage stagetulosta = new Stage();
+            stagetulosta.setScene(scenetulosta);
+            stagetulosta.setResizable(false);
+            stagetulosta.getIcons().add(new Image("/juoksija.png"));
+            stagetulosta.setTitle("Tulosta");
+            stagetulosta.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }    
+    }
     @FXML
     void handleLisaaLenkki() {
         try {
-            BorderPane ikkuna2 = (BorderPane)FXMLLoader.load(getClass().getResource("Lisaalenkki.fxml"));
-            Scene scene3 = new Scene(ikkuna2,324,485);
-            scene3.getStylesheets().add(getClass().getResource("lenkkikalenteri.css").toExternalForm());
-            Stage lisaalenkki = new Stage();
-            lisaalenkki.setScene(scene3);
-            lisaalenkki.setResizable(false);
-            lisaalenkki.getIcons().add(new Image("/juoksija.png"));
-            lisaalenkki.setTitle("Lisää lenkki");
-            lisaalenkki.show();
+            BorderPane panelisaalenkki = (BorderPane)FXMLLoader.load(getClass().getResource("Lisaalenkki.fxml"));
+            Scene scenelisaalenkki = new Scene(panelisaalenkki,324,485);
+            scenelisaalenkki.getStylesheets().add(getClass().getResource("lenkkikalenteri.css").toExternalForm());
+            Stage stagelisaalenkki = new Stage();
+            stagelisaalenkki.setScene(scenelisaalenkki);
+            stagelisaalenkki.setResizable(false);
+            stagelisaalenkki.getIcons().add(new Image("/juoksija.png"));
+            stagelisaalenkki.setTitle("Lisää lenkki");
+            stagelisaalenkki.show();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -47,8 +125,19 @@ public class LenkkikalenteriGUIController {
 
     @FXML
     void handleLisaaLaji() {
-        ModalController.showModal(KirjautuminenGUIController.class.getResource("Lisaalaji.fxml"),
-                "Lisää laji", null, "");
+        try {
+            BorderPane panelisaalaji = (BorderPane)FXMLLoader.load(getClass().getResource("Lisaalaji.fxml"));
+            Scene scenelisaalaji = new Scene(panelisaalaji,300,200);
+            scenelisaalaji.getStylesheets().add(getClass().getResource("lenkkikalenteri.css").toExternalForm());
+            Stage stagelisaalaji = new Stage();
+            stagelisaalaji.setScene(scenelisaalaji);
+            stagelisaalaji.setResizable(false);
+            stagelisaalaji.getIcons().add(new Image("/juoksija.png"));
+            stagelisaalaji.setTitle("Lisää uusi laji");
+            stagelisaalaji.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
     
     @FXML
